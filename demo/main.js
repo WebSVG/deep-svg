74817785047ae4aa86f680fb85af703ec94dd1e8
wassfila <wassim.filali@gmail.com>
2020-03-07 13:56:47 +0100

    simplified events and added text animations

--- demo/main.js
+++ demo/main.js
@@ -1,7 +1,18 @@
 import * as svgm from "../src/index.js";
 
 
+async function main(){
+    let svg = await svgm.createElement(document.body,{src:"../demo/diagram.svg",id:"diagram_a"});
+    console.log(`created svg element '${svg.id}'`);
+    window.addEventListener('text_click',onTextClick);
+}
 
-svgm.create(document.body,"../demo/diagram.svg")
-.then(console.log("done"));
+function onTextClick(e){
+    console.log(`main> '${e.detail.text}' text clicked on '${e.detail.svg.id}'`);
+    setTimeout(()=>{svgm.highlightText(e.detail.svg,e.detail.text,"opacity")}
+    ,1000)
+}
+
+main()
+.then(()=>console.log("main done"))
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 
-let state ={over_vertex:false,coord:{x:0,y:0},dragging:false,acting:false};
-
-let svg;
+let state ={coord:{x:0,y:0}};
 
 const config = {
     "scale_ratio":1.08,
@@ -9,36 +7,86 @@ const config = {
     "scale_max"  :5
 }
 
-async function create(parent,filename){
-    let res;
-    const response = await fetch(filename);
+const animations = {
+    "opacity":{attributeType:"CSS",attributeName:"opacity",from:"1",to:"0",dur:"500ms",repeatCount:"3"}
+};
+
+function defined(statement){
+    return (typeof(statement) != "undefined")
+}
+
+function sibling(sibl,element){
+    sibl.parentElement.appendChild(element);    
+}
+
+function send_event(event_name,data){
+	var event = new CustomEvent(event_name, {detail:data});
+	window.dispatchEvent(event);
+}
+
+function get_svg(target){
+    return (target.tagName == "svg")?target:target.closest("svg");
+}
+
+function create_xmlns(tagName,attributes){
+    const xmlns = "http://www.w3.org/2000/svg";
+    let element = document.createElementNS(xmlns,tagName);
+    for(let [key, value] of Object.entries(attributes)){
+        element.setAttribute(key,value);
+    }
+    return element;
+}
+
+async function createElement(parent,props){
+    const response = await fetch(props.src);
     const svg_text = await response.text();
     parent.insertAdjacentHTML("beforeend",svg_text)
     let elements = parent.getElementsByTagName("svg")
     let res_svg =  elements[elements.length-1];
+    if(defined(props.id)){
+        res_svg.id = props.id;
+    }else{
+        res_svg.id = `svg_${Math.round(1000000*Math.random())}`;
+    }
     svg_transform_init(res_svg);
     add_events(res_svg);
     return res_svg;
 }
 
-function add_events(l_svg){
-    svg = l_svg;
+function highlightText(svg,text,type="opacity"){
+    const tspans = svg.getElementsByTagName('tspan');
+    for(let tspan of tspans){
+        if(tspan.innerHTML == text){
+            let highlight = tspan.parentElement.querySelector("animate")
+            if(highlight == null){
+                highlight = create_xmlns("animate",animations[type]);
+                sibling(tspan,highlight);
+            }
+            highlight.beginElement();
+        }
+    }
+}
+
+function add_events(svg){
     svg.addEventListener( 'touchstart', onMousePan, false );
     svg.addEventListener( 'touchend', onMousePan, false );
     svg.addEventListener( 'mousedown', onMousePan, false );
-    svg.addEventListener( 'mouseup', onMousePan, false );
+    //svg.addEventListener( 'mouseup', onMousePan, false );
     svg.addEventListener( 'mousemove', onMousePan, false );
     svg.addEventListener( 'wheel', onWheel, false );
+//    svg.addEventListener( 'click', onClick, false );
     svg.addEventListener( 'contextmenu', onContext, false );
-
-    //onMouseVertex for every rect mouseenter,mouseleave
+}
+function onClick(e){
 }
 
 function onContext(e){
-    if(e.target.tagName == "rect"){
-        e.preventDefault();
-        e.stopPropagation();
+    if(e.target.tagName == "tspan"){
+        let svg = get_svg(e.target);
+        send_event("text_click",{svg:svg,text:e.target.innerHTML});
     }
+    e.preventDefault();
+    e.stopPropagation();
 }
 
 function onMousePan(e){
@@ -46,23 +94,9 @@ function onMousePan(e){
     let my = e.clientY;//e.offsetY
     let dx = mx - state.coord.x;
     let dy = my - state.coord.y;
-    if(state.dragging){
-        if(e.type == "mouseup"){
-            onViewMouse('drag',{start:false})
-            state.dragging = false;//for mouse up outside the vertex after starting a down inside
-        }
-    }
-    if((e.type != "mousemove") && (e.target.tagName == "rect")){
-        onMouseVertex(e);        
-    }
     if((e.buttons == 1) && (e.type == "mousemove")){
-        if(state.dragging){
-            onViewMouse('vert_move',{tx:dx,ty:dy});
-        }else if(e.target.tagName == "svg"){//svg or div
-            if(!state.dragging){
-                onViewMouse('view_move',{tx:dx,ty:dy});
-            }
-        }
+        let svg = get_svg(e.target);
+        svg_shift(svg,dx,dy);
     }
     state.coord.x = mx;
     state.coord.y = my;
@@ -70,96 +104,22 @@ function onMousePan(e){
     e.stopPropagation();
 }
 
-//coming from a registration in each rect to have mouseenter / mouseleave,...
-function onMouseVertex(e){
-    //console.log(`${e.type} on ${e.target.id}`);
-    const id = e.target.id.substr(5,e.target.id.length);
-    let graph_events = [];
-    let start;
-    if(['contextmenu', 'click'].includes(e.type)){
-        e.preventDefault();
-        e.stopPropagation();
-    }else if(['mousedown'].includes(e.type)){
-        if(e.buttons == 2){
-            graph_events.push('act');
-            start = true;
-            state.acting = true;
-        }else if(e.buttons == 1){
-            graph_events.push('drag');
-            start = true;
-            //console.log("drag start");
-            state.dragging = true;
-        }
-    }else if(['mouseup'].includes(e.type)){
-        if(state.dragging){
-            graph_events.push('drag');
-            state.dragging = false;
-            start = false;
-            //console.log("drag over");
-        }
-        if(state.acting){
-            graph_events.push('act');
-            start = false;
-            state.acting = false;
-        }
-    }else if(e.type == 'touchstart'){
-        if(e.touches.length == 1){
-            graph_events.push('hover');
-            start = true;
-            state.over_vertex = true;
-        }else if(e.touches.length == 2){
-            graph_events.push('act');
-            start = true;
-            graph_events.push('hover');
-            state.acting = true;
-            state.over_vertex = true;
-        }
-    }else if(e.type == 'mouseenter'){
-        graph_events.push('hover');
-        start = true;
-        state.over_vertex = true;
-    }else if(['mouseleave','touchend'].includes(e.type)){
-        graph_events.push('hover');
-        start = false;
-        state.over_vertex = false;
-        if(state.acting){
-            graph_events.push('act');
-            start = false;
-            state.acting = false;
-        }
-    }
-    graph_events.forEach(type => onViewMouse(type,{id:id,start:start}));
-    return false;
-}
-
 function onWheel(e){
     let step;
+    //console.log(e.offsetX)
     if(e.deltaY > 0){
         step = 'up';
     }else if (e.deltaY < 0){
         step = 'down';
     }
-    if(state.over_vertex){
-        onViewMouse('vertex_scale',{step:step});
-    }else{
-        let svg_rect_no_scale = e.target.parentElement.getBoundingClientRect();
-        let origin = {rx:e.offsetX / svg_rect_no_scale.width,ry:e.offsetY / svg_rect_no_scale.height};
-        onViewMouse('view_scale',{step:step,origin:origin});
-    }
+    let svg = get_svg(e.target);
+    let svg_rect_no_scale = svg.parentElement.getBoundingClientRect();
+    let origin = {rx:e.offsetX / svg_rect_no_scale.width, ry:e.offsetY / svg_rect_no_scale.height};
+    svg_scale(svg,step,origin);
     e.preventDefault();
     e.stopPropagation();
 }
 
-
-function onViewMouse(type,data){
-    //let svg = document.getElementById('svg_graph');
-    if(type == "view_scale"){
-        //svg_move(svg,0,0);
-        svg_scale(svg,data.step,data.origin);
-    }else if(type == "view_move"){
-        svg_shift(svg,data.tx,data.ty);
-    }
-}
 
 function svg_transform_init(element){
     element.style.backgroundColor = "#fafafa";
@@ -213,4 +173,4 @@ function svg_shift(element,tx,ty){
     }
 }
 
-export{create};
+export{createElement,highlightText};
